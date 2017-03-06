c3cdc52e08f1b8387e9fed7b74fa695a6ac18a58
Afaren <550047450@qq.com>
2017-03-06 13:47:19 +0800

    afaren - fixbug: modify to count_of_quizItem_in_db

--- collectFiles.js
+++ collectFiles.js
@@ -2,7 +2,7 @@ const glob = require('glob');
 const cp = require('cp');
 const dir_pattern = "2017*";
 const dest_dir = 'logic-puzzle';
-let count_of_current_quizItem_count_in_db = 7505;
+let count_of_current_quizItem_in_db = 7505;
 
 
 glob(dir_pattern, function (err, dirs) {
@@ -11,12 +11,12 @@ glob(dir_pattern, function (err, dirs) {
 
             files.forEach((file, index) => {
 
-                ++count_of_current_quizItem_count;
+                ++count_of_current_quizItem_in_db;
 
                 let jsonFile = file.replace('/', '_');
                 console.log(jsonFile)
                 // copy json files
-                cp(file, `${dest_dir}/${count_of_current_quizItem_count}.json`, (err, result) => {
+                cp(file, `${dest_dir}/${count_of_current_quizItem_in_db}.json`, (err, result) => {
                     if (err) {
                         throw err;
                     }
@@ -27,7 +27,7 @@ glob(dir_pattern, function (err, dirs) {
                 console.log(pngFile)
                 console.log(newPngFile)
                 // copy png files
-                cp(pngFile, `${dest_dir}/${count_of_current_quizItem_count}.png`, (err, result) => {
+                cp(pngFile, `${dest_dir}/${count_of_current_quizItem_in_db}.png`, (err, result) => {
                     if (err) {
                         throw err;
                     }
